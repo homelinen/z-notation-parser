6dd77aa05427669e780efe75ce8cb7e8bdcc43ad
Calum Gilchrist <calum@homelinen.org>
2013-01-22 20:49:00 +0000

    Added an Initial Set Implementation

--- tree.c
+++ tree.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const char l_br = '{'
+const char r_br = '}'
 // A node in the tree
 struct node {
     int key_value;
@@ -13,6 +15,29 @@ struct element {
     int key_value;
     struct element *next;
 };
+
+/**
+ * Recurse through the Set freeing memory
+ * from the end of the set
+ **/
+void destroy_set(struct element *el) {
+    if (el == 0) {
+        destroy_set(el->next);
+        free(el);
+    }
+}
+
+/** 
+ * Insert the key onto the end of the set
+ **/
+void insert_el(int key, struct element **el) {
+    if (*el == 0) {
+        *el = (struct element*) malloc( sizeof ( struct element ) );
+        (*el)->key_value = key;
+    } else {
+        insert_el(key, &((*el)->next));
+    }
+}
 
 /**
  * Recurse through tree, destroying all children up to leaves
@@ -43,9 +68,19 @@ void insert(int key, struct node **leaf) {
 }
 
 int main(int argc, char** argv) {
-    struct node *root = 0;
-    insert(8, &root );
+    int i;
 
-    printf("Root: %d", root->key_value);
+    struct node *x0 = 0;
+    insert(8, &x0 );
+
+    printf("x0 = %d\n", x0->key_value);
+
+    struct element *x1 = 0;
+    for (i = 1; i <= 7; i++) {
+      insert_el(i, &x1);
+    }
+    // Temporary, should be able to add trees to set
+    insert_el(x0->key_value, &x1);
+    printf("x1 = %d\n", x1->next->next->next->next->key_value);
     return 0;
 }
